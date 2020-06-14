8a3cfaced0459decf43b814f80d9b9fad4d73a18
RVohmin <vohmin77@gmail.com>
2020-06-14 10:13:45 +0300

    1.1. Состояние нити. [#304629]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -1,13 +1,19 @@
 package ru.job4j.concurrent;
 
+/**
+ * Cоздано две нити. Каждая нить должна вывести свое имя на консоль.
+ * Нить main должна дождаться завершения этих нитей и вывести на консоль сообщение, что работа завершена.
+ */
 public class ThreadState {
     public static void main(String[] args) throws InterruptedException {
         Thread first = new Thread(
                 () -> {
+                    System.out.println("Thread first");
                 });
         System.out.println("Thread first after created: " + first.getName() + " status: " + first.getState());
         Thread second = new Thread(
                 () -> {
+                    System.out.println("Thread second");
                 });
         System.out.println("Thread second after created: " + first.getName() + " status: " + second.getState());
         first.start();
