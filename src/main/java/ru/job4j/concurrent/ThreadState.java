98f550d40d1391a40243c61572b4dcc0db45a656
RVohmin <vohmin77@gmail.com>
2020-06-15 08:03:49 +0300

    1.1. Состояние нити. [#304629] fix2

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -8,27 +8,24 @@ public class ThreadState {
     public static void main(String[] args) throws InterruptedException {
         Thread first = new Thread(
                 () -> {
-                    System.out.println("Thread first worked");
+                    System.out.println("Thread first start");
                 });
         System.out.println("Thread first after created: " + first.getName() + " status: " + first.getState());
         Thread second = new Thread(
                 () -> {
-                    System.out.println("Thread second worked");
+                    System.out.println("Thread second start");
                 });
         System.out.println("Thread second after created: " + first.getName() + " status: " + second.getState());
         first.start();
         System.out.println("Thread first after start: " + first.getName() + " status: " + first.getState());
-        Thread.sleep(1000);
         second.start();
-        first.join();
         System.out.println("Thread second: after start: " + first.getName() + " status: " + second.getState());
-        second.join();
-        Thread.sleep(1000);
-        first.join();
-        second.join();
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            System.out.println("Thread 1 or 2 are worked");
+        }
+        System.out.println("main: " + Thread.currentThread().getState() + ", first & second threads are finished");
         System.out.println(first.getState());
         System.out.println(second.getState());
 
-        System.out.println("main: " + Thread.currentThread().getState() + ", first & second threads are finished");
     }
 }
