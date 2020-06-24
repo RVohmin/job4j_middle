2f3805a332e135a6ec85dd99af1d9739950a940f
RVohmin <vohmin77@gmail.com>
2020-06-24 17:07:02 +0300

    0. Управление нитью через wait. [#304626]

--- src/main/java/ru/job4j/wait/CountBarrier.java
+++ src/main/java/ru/job4j/wait/CountBarrier.java
@@ -1,20 +1,38 @@
 package ru.job4j.wait;
 
+/**
+ * Class described thread with counter. If counter == total, method awayte can be execute
+ */
 public class CountBarrier {
     private final Object monitor = this;
 
     private final int total;
-
+    private boolean flag = false;
     private volatile int count = 0;
 
     public CountBarrier(final int total) {
         this.total = total;
     }
 
+    public void on() {
+        synchronized (monitor) {
+            flag = true;
+            monitor.notifyAll();
+        }
+    }
+
+    public void off() {
+        synchronized (monitor) {
+            flag = false;
+            monitor.notifyAll();
+        }
+    }
+
     public void check() {
         synchronized (monitor) {
-            while (count != total) {
+            while (!flag) {
                 try {
+                    System.out.println(Thread.currentThread().getName() + " sleep");
                     monitor.wait();
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
@@ -26,6 +44,9 @@ public class CountBarrier {
 
     public synchronized void count() {
         count++;
+        if (count == total) {
+            on();
+        }
     }
 
     public void await() {
---
+++ src/test/java/ru/job4j/wait/CountBarrierTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.wait;
+
+import org.junit.Test;
+
+public class CountBarrierTest {
+    @Test
+    public void countTest() throws InterruptedException {
+
+        CountBarrier countBarrier = new CountBarrier(2);
+        Thread master = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName() + " started");
+                    countBarrier.await();
+                    System.out.println("Await from master finished");
+                },
+                "Master"
+        );
+        Thread slave = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName() + " started");
+                    countBarrier.count();
+                    try {
+                        System.out.println(Thread.currentThread().getName() + " sleep 3000");
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    countBarrier.count();
+                    System.out.println("Slave from slave finished");
+                },
+                "Slave"
+        );
+        master.start();
+        slave.start();
+        master.join();
+        slave.join();
+    }
+
+}
