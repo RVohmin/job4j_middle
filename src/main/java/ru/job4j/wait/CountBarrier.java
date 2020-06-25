cfc504979386fd519b7fb3e2cb50bcb3e16c0aa0
RVohmin <vohmin77@gmail.com>
2020-06-25 14:30:34 +0300

    0. Управление нитью через wait. [#304626]

--- src/main/java/ru/job4j/wait/CountBarrier.java
+++ src/main/java/ru/job4j/wait/CountBarrier.java
@@ -1,36 +1,30 @@
 package ru.job4j.wait;
 
 /**
- * Class described thread with counter. If counter == total, method awayte can be execute
+ * Class described thread with counter. If counter == total, method "Await" can be execute
  */
 public class CountBarrier {
     private final Object monitor = this;
 
+    /**
+     * outcome value for condition for put to sleep the threads
+     */
     private final int total;
-    private boolean flag = false;
+    /**
+     * counter
+     */
     private volatile int count = 0;
 
     public CountBarrier(final int total) {
         this.total = total;
     }
 
-    public void on() {
-        synchronized (monitor) {
-            flag = true;
-            monitor.notifyAll();
-        }
-    }
-
-    public void off() {
-        synchronized (monitor) {
-            flag = false;
-            monitor.notifyAll();
-        }
-    }
-
+    /**
+     * method checks if condition not true, than put to sleep the thread
+     */
     public void check() {
         synchronized (monitor) {
-            while (!flag) {
+            while (!(count == total)) {
                 try {
                     System.out.println(Thread.currentThread().getName() + " sleep");
                     monitor.wait();
@@ -42,13 +36,19 @@ public class CountBarrier {
         }
     }
 
+    /**
+     * Method increased count, if condition is met, waked all threads
+     */
     public synchronized void count() {
         count++;
         if (count == total) {
-            on();
+            monitor.notifyAll();
         }
     }
 
+    /**
+     * Method checked for count == total and put to sleep the thread if condition isn't met
+     */
     public void await() {
         check();
         System.out.println("Await run");
