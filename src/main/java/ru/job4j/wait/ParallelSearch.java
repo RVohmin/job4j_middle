45d0f0867dd215e56923f0bfe1fd3a6c447c7531
RVohmin <vohmin77@gmail.com>
2020-07-05 04:05:02 +0300

    2. Обеспечить остановку потребителя. [#304623]

--- src/main/java/ru/job4j/wait/ParallelSearch.java
+++ src/main/java/ru/job4j/wait/ParallelSearch.java
@@ -1,36 +1,28 @@
 package ru.job4j.wait;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 public class ParallelSearch {
-    public static void main(String[] args) {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
-        AtomicBoolean flag = new AtomicBoolean(true);
-
-        final Thread consumer = new Thread(
-                () -> {
-                    while (!queue.isEmptyQueue() || flag.get()) {
-                        Integer i = queue.poll();
-                        System.out.println(i);
-                    }
-                }, "Consumer"
-        );
-        consumer.start();
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(1);
 
         final Thread producer = new Thread(
                 () -> {
                     for (int index = 0; index != 3; index++) {
                         queue.offer(index);
-                        try {
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
                     }
-                    queue.wakeUpThreads();
-                    flag.set(false);
                 }, "Producer"
         );
+
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        Integer i = queue.poll();
+                    }
+                }, "Consumer"
+        );
         producer.start();
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
     }
 }
