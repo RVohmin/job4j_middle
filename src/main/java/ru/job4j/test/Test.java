441dafc82f3be4be934dddd03d1a71e7344c3338
RVohmin <vohmin77@gmail.com>
2020-08-07 20:35:14 +0300

    2. Валидация формы JS. [#304512]

--- src/main/java/ru/job4j/test/Test.java
+++ src/main/java/ru/job4j/test/Test.java
@@ -1,6 +1,6 @@
 package ru.job4j.test;
 
-import ru.job4j.synchronize.Count;
+import java.util.*;
 
 /**
  * ru.job4j.test
@@ -9,14 +9,45 @@ import ru.job4j.synchronize.Count;
  * @since 17.07.2020
  */
 public class Test {
-    public static void main(String[] args) throws InterruptedException {
-        Count count = new Count();
-        Thread first = new Thread(count::increment);
-        Thread second = new Thread(count::increment);
-        first.start();
-        second.start();
-        first.join();
-        second.join();
-        System.out.println(count.get());
+    static <T> List<T> merge(Iterator<T> i1, Iterator<T> i2, Comparator<T> comparator) {
+        List<T> list = new ArrayList<>();
+        T a = i1.next();
+        T b = i2.next();
+
+        while (true) {
+            if (comparator.compare(a, b) <= 0) {
+                list.add(a);
+                a = i1.next();
+            } else {
+                list.add(b);
+                b = i2.next();
+            }
+            if (!i1.hasNext()) {
+                list.add(b);
+                list.add(a);
+                i2.forEachRemaining(list::add);
+                break;
+            }
+            if (!i2.hasNext()) {
+                list.add(b);
+                list.add(a);
+                i1.forEachRemaining(list::add);
+                break;
+            }
+        }
+        i2.forEachRemaining(list::add);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> l1 = Arrays.asList(1, 5, 12, 66, 66, 67);
+        List<Integer> l2 = Arrays.asList(2, 3, 6, 7, 8, 12, 33, 66, 92, 134);
+        List<Integer> expected = Arrays.asList(1, 2, 3, 5, 6, 7, 8, 12, 12, 33, 66, 66, 66, 67, 92, 134);
+        List<Integer> merged1 = merge(l1.iterator(), l2.iterator(), Integer::compareTo);
+        System.out.println(expected.equals(merged1) ? "Test PASSED" : "Test FAILED");
+        System.out.println(merged1);
+        List<Integer> merged2 = merge(l2.iterator(), l1.iterator(), Integer::compareTo);
+        System.out.println(expected.equals(merged2) ? "Test PASSED" : "Test FAILED");
+        System.out.println(merged2);
     }
 }
